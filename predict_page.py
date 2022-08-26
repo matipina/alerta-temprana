242144cb4454bdcff72a9e9ca0a63ce8d1413b39
matipina <mlpina@uc.cl>
2022-08-26 01:35:18 -0400

    update predictions display

--- predict_page.py
+++ predict_page.py
@@ -3,7 +3,21 @@ import pickle
 import numpy as np
 import pandas as pd
 import sklearn
+import plotly
+from st_aggrid import AgGrid
 
+
+label = 'Aprobado (predicción)'
+
+def custom_style(row):
+    color = 'black'
+    if row[label] == False:
+        color = 'orange'
+    print(f'color: {color}')
+    print(f'row[label]: {row[label]}')
+    print(f'row:\n{row}')
+
+    return ['background-color: %s' % color]*len(row.values)
 
 def show_predict_page():
     st.write(
@@ -30,37 +44,41 @@ def show_predict_page():
             st.write(
                     '''
                     #### Selecciona las columnas a utilizar:
-                    *nota: debe ser un subset de ['tries', 'I1', 'T1', 'I2', 'T2']* 
+                    *nota: debe ser un subset de ['tries', 'T1', 'I1', 'I2', 'T2']* 
                     ''')
             
-            available_values = ['tries', 'I1', 'T1', 'I2', 'T2']
-            checked_values = []
+            available_values = ['tries', 'T1', 'I1', 'I2', 'T2']
             selected_values = {}
             for column in available_values:
                 selected_values[column] = st.checkbox(column, key=column)
 
             new_list = [value for value, selected in selected_values.items() if selected]
-            st.write(new_list)
-            print(new_list)
             next = st.button('Siguiente')
             num_selected = sum(selected_values.values())
 
-            if next:
-                selected_data = df.loc[:, new_list]
-                st.write(
-                    '''
-                    #### Predicciones:
-                    '''
-                )
-                model_folder = f'models/{code}'
+            if num_selected >= 1:
+                if next:
+                    selected_data = df.loc[:, new_list]
+                    st.write(
+                        '''
+                        #### Predicciones:
+                        '''
+                    )
+                    model_folder = f'models/{code}'
 
-                model_path = f'{model_folder}/model{num_selected-1}.sav'
-                model = pickle.load(open(model_path, 'rb'))
+                    model_path = f'{model_folder}/model{num_selected-1}.sav'
+                    model = pickle.load(open(model_path, 'rb'))
 
-                copy = selected_data.copy()
-                copy['En riesgo'] = model.predict(copy)
-                print(copy.head(10))
-                st.dataframe(data=copy)
+                    copy = selected_data.copy()
+                    copy[label] = model.predict(copy)
+                    #print(copy)
+                    #st.dataframe(data=copy)
+                    #st.plotly_chart(copy)
+                    AgGrid(copy.style.apply(custom_style, axis=1))
+                    #st.dataframe(copy.style.apply(custom_style, axis=1))
+            else:
+                st.warning('Debes seleccionar al menos un atributo.')
+                next = False
 
 
 
