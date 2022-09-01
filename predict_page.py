749d03103c9b9fae7dda19fab3c5ff3f8b986d7e
matipina <mlpina@uc.cl>
2022-09-01 00:10:45 -0400

    added ICH1104 models

--- predict_page.py
+++ predict_page.py
@@ -5,6 +5,11 @@ import pandas as pd
 import sklearn
 
 label = 'Aprobado'
+course_variables = {
+    'IIC1103': ['tries', 'T1', 'I1', 'T2', 'I2'],
+    'IIC2233': [],
+    'ICH1104': ['T1', 'I1', 'T2', 'I2', 'T3']
+}
 
 def custom_style(row):
     color = st.get_option('theme.backgroundColor') # get color corresponding to current theme
@@ -22,7 +27,9 @@ def show_predict_page():
     courses = (
         'Selecciona',
         'IIC1103: Introducción a la Programación',
-        'IIC2233: Programación Avanzada'
+        'IIC2233: Programación Avanzada',
+        'ICH1104: Mecánica de fluidos',
+        'ICS1113: Optimización',    
     )
 
     course = st.selectbox("Selecciona un curso:", courses)
@@ -31,21 +38,32 @@ def show_predict_page():
 
         st.write(f'#### {course}')
         code = course[:7]
-        file = st.file_uploader("Carga un archivo (.csv) con las notas del semestre.")
-        
+        model_folder = f'models/{code}'
+
+        file = st.file_uploader("Carga un archivo (.csv o .xlsx) con las notas del semestre.")
         if file:
-            df = pd.read_csv(file)
+            filename = file.name
+            if filename.endswith('.csv'):
+                df = pd.read_csv(file)
+            elif filename.endswith('.xlsx') or filename.endswith('.xls'):
+                df = pd.read_excel(file)
+            else:
+                st.warning('Tipo de archivo no válido. Inténtalo de nuevo.')
+
             st.write(
-                    '''
-                    #### Selecciona las columnas a utilizar:
-                    *nota: debe ser un subset de ['tries', 'T1', 'I1', 'I2', 'T2']* 
+                    f'''
+                    #### Selecciona las columnas a utilizar para las predicciones:
+                    *nota: debe ser un subset de {course_variables[code]}* 
                     ''')
             
-            available_values = ['tries', 'T1', 'I1', 'I2', 'T2']
+            available_values = course_variables[code]
             selected_values = {}
+
+            # Creamos un checkbox para cada valor disponible
             for column in available_values:
                 selected_values[column] = st.checkbox(column, key=column)
 
+            # Creamos una lista con los valores seleccionados en las checkboxes
             new_list = [value for value, selected in selected_values.items() if selected]
             next = st.button('Siguiente')
             num_selected = sum(selected_values.values())
@@ -58,14 +76,25 @@ def show_predict_page():
                         #### Predicciones:
                         '''
                     )
-                    model_folder = f'models/{code}'
 
                     model_path = f'{model_folder}/model{num_selected-1}.sav'
                     model = pickle.load(open(model_path, 'rb'))
 
                     copy = selected_data.copy()
-                    copy[label] = model.predict(copy)
-                    st.dataframe(copy.style.apply(custom_style, axis=1))
+                    clean_data = copy.dropna(subset=new_list)
+                    clean_data[label] = model.predict(clean_data)
+
+                    # Agregar datos para identificar a los estudiantes
+                    if any(i in df.columns for i in ('id', 'ID', 'Id')):
+                        student_data = df.loc[:, df.columns.isin(('id', 'ID', 'Id'))]
+                    else:
+                        student_data = df.iloc[:, 0]
+                    final_data = clean_data.merge(student_data, left_index=True, right_index=True)
+
+                    cols = student_data.columns.tolist() + clean_data.columns.tolist()
+                    final_data = final_data[cols]
+                    
+                    st.dataframe(final_data.style.apply(custom_style, axis=1))
             else:
                 st.warning('Debes seleccionar al menos un atributo.')
                 next = False
